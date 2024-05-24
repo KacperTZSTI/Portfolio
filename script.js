65d66e8d38993fedc4b79e89254e84c1ac602068
KacperTZSTI <113526043+KacperTZSTI@users.noreply.github.com>
2024-05-24 06:17:10 +0000

    script cleanup

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let header = document.querySelector('header');
 let title = document.querySelector('#title');
 let video = document.querySelector('video')
 
-console.log(window.innerWidth);
 
 window.onscroll = function() {
   title.style.top = window.scrollY*0.48 + "px";
